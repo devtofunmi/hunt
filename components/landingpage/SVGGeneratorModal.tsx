fa73b931785cb818d7ba1ffecea6543e225bd467
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-30 14:42:51 +0100

    ..

--- components/landingpage/SVGGeneratorModal.tsx
+++ components/landingpage/SVGGeneratorModal.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import SVGGenerator from "../SVGGenerator";
-import { GrClose } from "react-icons/gr";
+'use client';
 
+import React from 'react';
+import SVGGenerator from '../SVGGenerator';
+import { GrClose } from 'react-icons/gr';
 
-type Props = {
+interface SVGGeneratorModalProps {
   isOpen: boolean;
   onClose: () => void;
-};
+}
 
-const SVGGeneratorModal = ({ isOpen, onClose }: Props) => {
+const SVGGeneratorModal: React.FC<SVGGeneratorModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-50 bg-black/70 flex items-center justify-center text-[#171717]">
-      <div className="bg-[#18181b]  rounded-xl w-[85%] md:w-full max-w-3xl p-2 md:p-6 relative">
+      <div className="bg-[#18181b] rounded-xl w-[85%] md:w-full max-w-3xl p-2 md:p-6 relative">
         <button
           onClick={onClose}
-          className="absolute cursor-pointer top-4 right-4 text-gray-600 hover:text-black"
+          className="absolute top-4 right-4 text-gray-600 hover:text-black cursor-pointer"
+          aria-label="Close modal"
         >
           <GrClose size={20} />
         </button>
