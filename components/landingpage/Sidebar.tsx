0c01d1d3cc5d5ba17dfe8dd926d0e61442b2b2e2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-30 14:42:35 +0100

    clean up code

--- components/landingpage/Sidebar.tsx
+++ components/landingpage/Sidebar.tsx
@@ -1,23 +1,28 @@
+'use client';
+
 import React from 'react';
 import ProductForm from './ProductForm';
 
-const Sidebar = ({ onOpenModal }: { onOpenModal: () => void }) => {
-  return (
-    <aside className="">
-  <div className="sticky top-24 md:w-[400px] flex flex-col gap-8">
-    <div className="w-full hidden md:block">
-      <input
-        type="text"
-        placeholder="Search a product"
-        className="w-full px-4 py-2 border rounded-full border-[#27272a] cursor-pointer"
-        onClick={onOpenModal}
-        readOnly
-      />
-    </div>
-    <ProductForm />
-  </div>
-</aside>
+interface SidebarProps {
+  onOpenModal: () => void;
+}
 
+const Sidebar: React.FC<SidebarProps> = ({ onOpenModal }) => {
+  return (
+    <aside>
+      <div className="sticky top-24 md:w-[400px] flex flex-col gap-8">
+        <div className="w-full hidden md:block">
+          <input
+            type="text"
+            placeholder="Search a product"
+            className="w-full px-4 py-2 border rounded-full border-[#27272a] cursor-pointer"
+            onClick={onOpenModal}
+            readOnly
+          />
+        </div>
+        <ProductForm />
+      </div>
+    </aside>
   );
 };
 
