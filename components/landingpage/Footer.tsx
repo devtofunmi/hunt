0e8c125474be1bfc872aae16835b1667f839c6a6
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-04-30 14:41:14 +0100

    footer component

---
+++ components/landingpage/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <div className="py-6 px-6 text-white text-center">
+        <p className="text-sm text-gray-400">&copy; 2025 LaunchHunt. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
