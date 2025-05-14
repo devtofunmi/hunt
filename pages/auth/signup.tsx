cd6e942095c29d4de144eae5b38d60ba1e3f158b
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-14 09:56:29 +0100

    cleanup code

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -66,7 +66,7 @@ const SignupPage: React.FC = () => {
     }
 
     try {
-      const res = await axios.post('https://hunt.up.railway.app/auth/signup', {
+      const res = await axios.post('https://launchhunt.up.railway.app/auth/signup', {
         username,
         email,
         password,
