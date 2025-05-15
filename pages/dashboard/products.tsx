bbf5f6d2acb8e45ed775d9b5ed9d00ad05810080
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-15 12:38:15 +0100

    ....

--- pages/dashboard/products.tsx
+++ pages/dashboard/products.tsx
@@ -75,7 +75,7 @@ const ProductsPage: React.FC = () => {
             <h2 className="text-xl font-semibold">Your Products</h2>
             <Link href="/dashboard/createproduct">
               <button className="flex items-center cursor-pointer gap-2 bg-gradient-to-r from-[#6E00FF] to-[#0096FF] text-white px-2 md:px-4 py-2 rounded-md">
-                <FiPlus />
+                <FiPlus className="hidden md:block" />
                 Create New Product
               </button>
             </Link>
